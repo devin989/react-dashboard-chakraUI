1a1fa362df4f0d82edcb6cc0d64e0acc19776746
devin989 <deveenrath@gmail.com>
2023-04-07 15:09:30 +0530

    add donut chart

--- src/views/admin/default/index.tsx
+++ src/views/admin/default/index.tsx
@@ -1,59 +1,24 @@
-/*!
-  _   _  ___  ____  ___ ________  _   _   _   _ ___   
- | | | |/ _ \|  _ \|_ _|__  / _ \| \ | | | | | |_ _| 
- | |_| | | | | |_) || |  / / | | |  \| | | | | || | 
- |  _  | |_| |  _ < | | / /| |_| | |\  | | |_| || |
- |_| |_|\___/|_| \_\___/____\___/|_| \_|  \___/|___|
-                                                                                                                                                                                                                                                                                                                                       
-=========================================================
-* Horizon UI - v1.1.0
-=========================================================
-
-* Product Page: https://www.horizon-ui.com/
-* Copyright 2022 Horizon UI (https://www.horizon-ui.com/)
-
-* Designed and Coded by Simmmple
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-
 // Chakra imports
-import {
-  Box,
-  Button,
-  Flex,
-  Icon,
-  Grid,
-  Link,
-  Text,
-  useColorModeValue,
-  SimpleGrid,
-} from "@chakra-ui/react";
+import { Box, Flex, Grid, SimpleGrid } from "@chakra-ui/react";
 
 // Custom components
 import Banner from "views/admin/default/components/Banner";
 
 import DailyTraffic from "views/admin/default/components/DailyTraffic";
-import PieCard from "views/admin/default/components/PieCard";
+import DonutCard from "views/admin/default/components/DonutCard";
 import WeeklyRevenue from "views/admin/default/components/WeeklyRevenue";
 
 import MiniStatistics from "components/card/MiniStatistics";
+import LineChart from "components/charts/LineChart";
 import Card from "components/card/Card";
-import IconBox from "components/icons/IconBox";
+
 import {
-  MdAddTask,
-  MdAttachMoney,
-  MdBarChart,
-  MdFileCopy,
-} from "react-icons/md";
+  lineChartDataTotalSpent,
+  lineChartOptionsTotalSpent,
+} from "variables/charts";
 
 export default function UserReports() {
   // Chakra Color Mode
-  const brandColor = useColorModeValue("brand.500", "white");
-  const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
   return (
     <Box pt={{ base: "180px", md: "80px", xl: "80px" }}>
       {/* Main Fields */}
@@ -80,34 +45,36 @@ export default function UserReports() {
       </Grid>
       <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} gap="20px" mb="20px">
         <MiniStatistics
-          startContent={
-            <IconBox
-              w="56px"
-              h="56px"
-              bg={boxBg}
-              icon={
-                <Icon w="32px" h="32px" as={MdBarChart} color={brandColor} />
-              }
+          endContent={
+            <LineChart
+              chartData={lineChartDataTotalSpent}
+              chartOptions={lineChartOptionsTotalSpent}
             />
           }
           name="Earnings"
           value="$350.4"
         />
         <MiniStatistics
-          startContent={
-            <IconBox
-              w="56px"
-              h="56px"
-              bg={boxBg}
-              icon={
-                <Icon w="32px" h="32px" as={MdAttachMoney} color={brandColor} />
-              }
+          endContent={
+            <LineChart
+              chartData={lineChartDataTotalSpent}
+              chartOptions={lineChartOptionsTotalSpent}
             />
           }
           name="Spend this month"
           value="$642.39"
         />
-        <MiniStatistics growth="+23%" name="Sales" value="$574.34" />
+        <MiniStatistics
+          endContent={
+            <LineChart
+              chartData={lineChartDataTotalSpent}
+              chartOptions={lineChartOptionsTotalSpent}
+            />
+          }
+          growth="+23%"
+          name="Sales"
+          value="$574.34"
+        />
       </SimpleGrid>
       <Grid
         mb="20px"
@@ -125,8 +92,8 @@ export default function UserReports() {
           flexDirection="column"
           gridArea={{ xl: "1 / 3 / 2 / 4", "2xl": "1 / 2 / 2 / 3" }}
         >
-          <Card px="0px" mb="20px">
-            <PieCard />
+          <Card px="0px" mb="20px" h="100%">
+            <DonutCard growth="+20%" />
           </Card>
         </Flex>
       </Grid>
