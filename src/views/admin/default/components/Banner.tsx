558dc632254b8cb07fd8dff7edcbf1dc37ab2352
devin989 <deveenrath@gmail.com>
2023-04-07 17:56:19 +0530

    add banner

--- src/views/admin/default/components/Banner.tsx
+++ src/views/admin/default/components/Banner.tsx
@@ -1,71 +1,61 @@
 // Chakra imports
-import { Button, Flex, Link, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Link, Text, Image } from "@chakra-ui/react";
 
 // Assets
-import banner from 'assets/img/nfts/NftBanner1.png';
+import banner from "assets/img/banner/banner1.png";
 
 export default function Banner() {
-	// Chakra Color Mode
-	return (
-		<Flex
-			direction='column'
-			bgImage={banner}
-			bgSize='cover'
-			py={{ base: '30px', md: '56px' }}
-			px={{ base: '30px', md: '64px' }}
-			borderRadius='30px'>
-			<Text
-				fontSize={{ base: '24px', md: '34px' }}
-				color='white'
-				mb='14px'
-				maxW={{
-					base: '100%',
-					md: '64%',
-					lg: '46%',
-					xl: '70%',
-					'2xl': '50%',
-					'3xl': '42%'
-				}}
-				fontWeight='700'
-				lineHeight={{ base: '32px', md: '42px' }}>
-				Discover, collect, and sell extraordinary NFTs
-			</Text>
-			<Text
-				fontSize='md'
-				color='#E3DAFF'
-				maxW={{
-					base: '100%',
-					md: '64%',
-					lg: '40%',
-					xl: '56%',
-					'2xl': '46%',
-					'3xl': '34%'
-				}}
-				fontWeight='500'
-				mb='40px'
-				lineHeight='28px'>
-				Enter in this creative world. Discover now the latest NFTs or start creating your own!
-			</Text>
-			<Flex align='center'>
-				<Button
-					bg='white'
-					color='black'
-					_hover={{ bg: 'whiteAlpha.900' }}
-					_active={{ bg: 'white' }}
-					_focus={{ bg: 'white' }}
-					fontWeight='500'
-					fontSize='14px'
-					py='20px'
-					px='27'
-					me='38px'>
-					Discover now
-				</Button>
-				<Link>
-					<Text color='white' fontSize='sm' fontWeight='500'>
-						Watch video
-					</Text>
-				</Link>
-			</Flex>
-		</Flex>
-	);
+  return (
+    <Flex
+      direction={{ base: "column", md: "row" }}
+      justify="space-between"
+      align={{ base: "center", md: "flex-start" }}
+      bg="#2BEBC8"
+      py={{ base: "30px", md: "96px" }}
+      px={{ base: "30px", md: "64px" }}
+      borderRadius="30px"
+      position="relative"
+    >
+      <Box position="absolute" right="50px" top="90px">
+        <Image src={banner} alt="Your image" boxSize="200px" objectFit="fill" />
+      </Box>
+      <Box>
+        <Text
+          fontSize={{ base: "24px", md: "28px" }}
+          color="black"
+          mb="14px"
+          maxW={{
+            base: "100%",
+            md: "64%",
+            lg: "46%",
+            xl: "70%",
+            "2xl": "50%",
+            "3xl": "42%",
+          }}
+          fontWeight="700"
+          lineHeight={{ base: "32px", md: "42px" }}
+        >
+          Welcome to your dashboard!
+        </Text>
+        <Text
+          fontSize="md"
+          color="black"
+          maxW={{
+            base: "100%",
+            md: "64%",
+            lg: "40%",
+            xl: "56%",
+            "2xl": "46%",
+            "3xl": "34%",
+          }}
+          fontWeight="500"
+          mb="40px"
+          lineHeight="28px"
+        >
+          Try our new Admin Dashboard Template, build with live Ant-Design
+          components. Customize it to your needs and release to production!
+        </Text>
+      </Box>
+    </Flex>
+  );
 }
